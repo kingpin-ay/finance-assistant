6ae7d49a916c6d6f6adccd154d52f190b681442b
kingpin-ay <ayushmondal001@gmail.com>
2024-10-21 01:26:56 +0530

    feat: setup the db and also the connection

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,24 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"backend/initializer"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializer.LoadEnv()
+	initializer.InitDatabaseConnection()
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	server := gin.Default()
+
+	server.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+
+	server.Run()
 }
