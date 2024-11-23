dfd5fd5783d34264d90b6a877ff7109534780cf7
kingpin-ay <ayushmondal001@gmail.com>
2024-11-23 19:10:45 +0530

    fix: moved the protection code to root page.tsx

--- frontend/app/main/layout.tsx
+++ frontend/app/main/layout.tsx
@@ -1,16 +1,6 @@
-"use client";
-import { useUser } from "@/utils/contexts/AuthContext";
-import { redirect } from "next/navigation";
-import React, { useLayoutEffect } from "react";
+import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  const { user } = useUser();
-  useLayoutEffect(() => {
-    if (!user || !user.email) {
-      // Redirect to the login page if user data is missing
-      redirect("/auth/login");
-    }
-  }, [user]);
   return <div>{children}</div>;
 };
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,18 @@
+"use client";
+import { useUser } from "@/utils/contexts/AuthContext";
+import { redirect } from "next/navigation";
+import { useLayoutEffect } from "react";
 
 export default function Home() {
+  const { user } = useUser();
+  useLayoutEffect(() => {
+    console.log("user", user);
+    if (!user || !user.email) {
+      // Redirect to the login page if user data is missing
+      redirect("/auth/login");
+    } else {
+      redirect("/main/dashboard");
+    }
+  }, [user]);
   return <div></div>;
 }
